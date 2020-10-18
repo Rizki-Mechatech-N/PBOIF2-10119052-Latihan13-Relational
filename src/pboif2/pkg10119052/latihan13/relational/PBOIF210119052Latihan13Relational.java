ccbc4364ad997471768c10d086504ffde1f6cf31
Kiki <Kiki@Kiki-PC>
2020-10-18 11:51:05 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan13/relational/PBOIF210119052Latihan13Relational.java
@@ -0,0 +1,26 @@
+package pboif2.pkg10119052.latihan13.relational;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * operasi relational
+ * 
+ */
+public class PBOIF210119052Latihan13Relational {
+
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil2 >= bil1));
+        System.out.println("a <= b = " + (bil2 <= bil1));
+        
+    }
+    
+}
